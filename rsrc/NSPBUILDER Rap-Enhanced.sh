5517a718b2f94f9ed21b9f3f96c803b32261dabd
Devin <28ddhpf1@anonaddy.com>
2026-07-29 18:05:34 -0400

    Add WIP Workflows

---
+++ rsrc/NSPBUILDER Rap-Enhanced.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+nton build "../raptor-consoles-enhanced.nro" --sdmc "/switch/raptor-enhanced.nro" --name "Raptor NX" --publisher "RetroGamer02" --version "1.0.0" --icon "raptornxicon.jpg"
+echo "Finished! NSP has been built!"
